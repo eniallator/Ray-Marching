4763e605854bf67078a63132cd17be66272bb2cc
eniallator <eniallatormc@gmail.com>
2020-05-09 00:53:50 +0100

    Made the light follow a random path that takes 10 seconds to complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+const simplex = new SimplexNoise(new Date().getTime());
+
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
@@ -29,25 +31,49 @@ const scene = new Scene(0, 0, canvas.width, canvas.height);
 // const numRays = 2880;
 const numRays = 20;
 
+const timeToRepeat = 10000;
+const noiseScale = 0.1;
+let prevTime = new Date().getTime();
+let currTime = 0;
+
 function run() {
-  if (mouseMoved) {
-    ctx.fillStyle = "black";
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
-    ctx.fillStyle = "white";
+  // if (mouseMoved) {
+  const percentRound = currTime / timeToRepeat;
+  ctx.fillStyle = "black";
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+  ctx.fillStyle = "white";
 
-    scene.draw(ctx);
+  scene.draw(ctx);
 
-    for (let i = 0; i < numRays; i++) {
-      const angle = (i * Math.PI * 2) / numRays;
-      const dir = new Vector(Math.sin(angle), Math.cos(angle));
+  for (let i = 0; i < numRays; i++) {
+    const angle = Math.PI / 4 + (i * Math.PI * 2) / numRays;
+    const dir = new Vector(Math.sin(angle), Math.cos(angle));
+    // const position = mouse;
+    const position = new Vector(
+      (simplex.noise2D(
+        noiseScale * Math.sin(percentRound * Math.PI * 2),
+        noiseScale * Math.cos(percentRound * Math.PI * 2)
+      ) +
+        1) /
+        2,
+      (simplex.noise2D(
+        noiseScale * Math.sin(percentRound * Math.PI * 2) + 10000,
+        noiseScale * Math.cos(percentRound * Math.PI * 2) + 10000
+      ) +
+        1) /
+        2
+    ).multiply(new Vector(canvas.width, canvas.height));
 
-      const ray = new Ray(mouse, dir);
-      ray.cast(scene);
-      ray.draw(ctx);
-    }
-
-    mouseMoved = false;
+    const ray = new Ray(position, dir);
+    ray.cast(scene);
+    ray.draw(ctx);
   }
+
+  //   mouseMoved = false;
+  // }
+  const now = new Date().getTime();
+  currTime = (currTime + now - prevTime) % timeToRepeat;
+  prevTime = now;
 
   requestAnimationFrame(run);
 }
